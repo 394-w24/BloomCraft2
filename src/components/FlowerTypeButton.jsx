d8d171577574d3c701eef193284d34594b0f25ab
theogguu <38506698+theogguu@users.noreply.github.com>
2024-02-21 16:41:33 -0600

    small fix: icons properly show on live server

--- src/components/FlowerTypeButton.jsx
+++ src/components/FlowerTypeButton.jsx
@@ -9,9 +9,9 @@ const FlowerTypeButton = ({ flowerType, setFlowerType, value }) => {
   return (
     <div className={`flower-type-button ${flowerType === value ? 'highlighted-button' : ''}`} onClick={handleClick}>
       <img className="flower-type-icon"
-        src={value === "Focal" ? "/public/icons/focal.png"
-          : value === "Filler" ? "/public/icons/filler.png"
-            : "/public/icons/foliage.png"}
+        src={value === "Focal" ? "/icons/focal.png"
+          : value === "Filler" ? "/icons/filler.png"
+            : "/icons/foliage.png"}
         alt={value}
       />
     </div>
