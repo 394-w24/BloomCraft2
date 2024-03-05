c7f66eef18fabb5653fa56f2f5261cadb52f9ef3
mercykenya <85264572+mercykenya@users.noreply.github.com>
2024-03-05 16:09:02 -0600

    Some styling on the note input

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,18 +14,23 @@ const NoteComponent = ({ notes, setGenerateNoteView }) => {
 
   return (
     <div>
-      <h1 style={{ fontSize: "20px", marginBottom: "15px" }}>Enter text below or use our Note Generator to create a heartfelt note based on the flowers you selected.</h1>
+     <h1 style={{ fontSize: "20px", marginBottom: "15px", fontWeight: "bold" }}>Enter text below or use our Note Generator to create a heartfelt note based on the flowers you selected.</h1>
       <div>
         <button style={{ marginBottom: "20px", background: "rgb(213, 240, 164)", border: "none", borderRadius: "20px", padding: "5px 30px" }}
           onClick={handleGenerateNote}>Generate Note</button>
       </div>
       <textarea
-        placeholder="Write your note here..."
+        placeholder="Note text goes here..."
         value={note}
         onChange={handleNoteChange}
         rows={6}
         cols={40}
-        style={{ marginBottom: "10px" }}
+        style={{
+          marginBottom: "10px",
+          background: "rgb(213, 240, 164)",
+          borderRadius: "5px",
+          padding: "5px",
+        }}
       />
     </div>
   );
