07d729d7852a77b5700bb87776f91fd6c86ad34b
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2024-03-12 22:21:11 -0500

    Vtest done victor

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -14,10 +14,26 @@ const NoteComponent = ({ notes, setGenerateNoteView }) => {
 
   return (
     <div>
-     <h1 style={{ fontSize: "20px", marginBottom: "15px", fontWeight: "bold" }}>Enter text below or use our Note Generator to create a heartfelt note based on the flowers you selected.</h1>
+      <h1
+        style={{ fontSize: "20px", marginBottom: "15px", fontWeight: "bold" }}
+      >
+        Enter text below or use our Note Generator to create a heartfelt note
+        based on the flowers you selected.
+      </h1>
       <div>
-        <button style={{ marginBottom: "20px", background: "rgb(213, 240, 164)", border: "none", borderRadius: "20px", padding: "5px 30px" }}
-          onClick={handleGenerateNote}>Generate Note</button>
+        <button
+          style={{
+            marginBottom: "20px",
+            background: "rgb(213, 240, 164)",
+            border: "none",
+            borderRadius: "20px",
+            padding: "5px 30px",
+          }}
+          onClick={handleGenerateNote}
+          data-testid="dropdowntest"
+        >
+          Generate Note
+        </button>
       </div>
       <textarea
         placeholder="Note text goes here..."
